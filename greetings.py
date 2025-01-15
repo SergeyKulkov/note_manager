fb69e7514be87548ab3236fe8fc01f927e7afaac
SergeyKulkov <kulkov_2006@mail.ru>
2025-01-15 21:26:36 +0300

    greetings.py

--- greetings.py
+++ greetings.py
@@ -1,7 +1,7 @@
 username = "Sergey Kulkov"
 title = "Homework"
 content = "Grade 1. Task 2."
-status = False # Заметка еще не выполнена
+status = "Не выполнена" # Заметка еще не выполнена
 created_date = "13-01-2025"
 issue_date = "15-01-2025"
 
